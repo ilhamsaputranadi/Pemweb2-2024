3f238358ee33dbbd142eb9e363e27aa5ac94cf8c
ilhamsaputranadi <ilhamsaputra200418@gmail.com>
2024-02-27 12:24:28 +0700

    first commit

---
+++ praktikum 1/array_asosiatif.php
@@ -0,0 +1,10 @@
+<?php
+$mahasiswaDUA = 
+['nama' => 'ilham saputra',
+'nim' => '0110123028',
+'jurusan' => 'sistem informasi',
+];
+//memanggil array asosiatif
+echo 'nama:'  . $mahasiswaDUA["nama"] . "<br>";
+echo 'nim:'  . $mahasiswaDUA["nim"]  . "<br>";
+echo 'jurusan:'  . $mahasiswaDUA["jurusan"];
---
+++ praktikum 1/variabel.php
@@ -0,0 +1,8 @@
+<?php
+// membuat variabel
+$mahasiswasi10 = ["ilham saputra","sistem informasi",'0110123028'];
+
+//calling variabel
+echo 'nama; ' . $mahasiswasi10[0] . "<br>"; //concatenation
+echo "jurusan: $mahasiswasi10[1] <br>"; //string interpolation
+echo "ips semester 1:" . $mahasiswasi10[2];
